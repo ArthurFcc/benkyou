8035bd7d8060d377c66a45ab0e1f9d0a756432e1
Arthur Faccio <arthur.faccio@proton.me>
2025-11-21 13:12:09 -0300

    Many things

--- lib/drawing_canvas.dart
+++ lib/drawing_canvas.dart
@@ -1,6 +1,8 @@
 import 'package:benkyou/painters/dotted_cross_painter.dart';
 import 'package:benkyou/painters/painter.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:step_progress/step_progress.dart';
 
 class DrawingCanvas extends StatefulWidget {
   const DrawingCanvas({super.key});
@@ -28,6 +30,11 @@ class _DrawingCanvasState extends State<DrawingCanvas> {
     }
   }
 
+  StepProgressController controller = StepProgressController(
+    totalSteps: 5,
+    initialStep: 3,
+  );
+
   @override
   Widget build(BuildContext context) {
     final canvasSize = Size(
@@ -37,33 +44,53 @@ class _DrawingCanvasState extends State<DrawingCanvas> {
 
     return Scaffold(
       appBar: AppBar(
-        title: const Text("勉強 | Benkyou"),
-        actions: [
-          IconButton(
-            onPressed: () => _undo(),
-            icon: const Icon(Icons.arrow_back_ios_new_rounded),
-          ),
-          IconButton(onPressed: () => _clear(), icon: const Icon(Icons.delete)),
-        ],
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            StepProgress(
+              totalSteps: 5,
+              controller: controller,
+              visibilityOptions: StepProgressVisibilityOptions.lineOnly,
+              theme: const StepProgressThemeData(
+                stepLineStyle: StepLineStyle(
+                  lineThickness: 10,
+                  borderRadius: Radius.circular(6),
+                ),
+                activeForegroundColor: Colors.green,
+                defaultForegroundColor: Colors.grey,
+              ),
+            ),
+            Icon(Icons.star, size: 21, color: Colors.amberAccent),
+          ],
+        ),
       ),
+      persistentFooterAlignment: AlignmentDirectional.center,
+      persistentFooterButtons: [
+        FilledButton.icon(
+          onPressed: () {},
+          label: Text("Voltar"),
+          style: ButtonStyle(
+            backgroundColor: WidgetStatePropertyAll(Colors.blueGrey),
+          ),
+          icon: Icon(Icons.arrow_back),
+        ),
+        SizedBox(width: 150),
+        FilledButton.icon(
+          onPressed: () {},
+          label: Text("Avancar"),
+          icon: Icon(Icons.arrow_forward),
+          style: ButtonStyle(
+            backgroundColor: WidgetStatePropertyAll(Colors.blueGrey),
+          ),
+          iconAlignment: IconAlignment.end,
+        ),
+      ],
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 24),
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 24),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                spacing: 12,
-                children: [
-                  Text("あ", style: TextStyle(fontSize: 82)),
-                  Text("あお", style: TextStyle(fontSize: 32)),
-                  CircleAvatar(radius: 16, backgroundColor: Colors.blue),
-                ],
-              ),
-            ),
-
             GestureDetector(
               onPanStart: (details) => setState(() {
                 paths.add([details.localPosition]);
@@ -71,33 +98,86 @@ class _DrawingCanvasState extends State<DrawingCanvas> {
               onPanUpdate: (details) => setState(() {
                 paths.last.add(details.localPosition);
               }),
-              child: Container(
-                width: canvasSize.width,
-                height: canvasSize.height,
-                decoration: BoxDecoration(
-                  shape: BoxShape.rectangle,
-                  color: Colors.white30,
-                  borderRadius: BorderRadius.circular(12),
-                  border: Border.all(color: Colors.black26, width: 2),
-                  backgroundBlendMode: BlendMode.clear,
-                ),
+              child: SizedBox(
                 child: CustomPaint(
                   painter: Painter(paths),
                   size: canvasSize,
                   child: Stack(
-                    alignment: AlignmentGeometry.center,
+                    alignment: Alignment.center,
                     children: [
-                      CustomPaint(
-                        painter: DottedCrossPainter(),
-                        size: Size.infinite,
-                      ),
                       Text(
-                        "あ",
-                        style: TextStyle(fontSize: 180, color: Colors.black26),
+                        "人",
+                        style: GoogleFonts.kleeOne(
+                          fontSize: 180,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black26,
+                        ),
                       ),
                     ],
                   ),
                 ),
+              ),
+            ),
+            GestureDetector(
+              onPanStart: (details) => setState(() {
+                paths.add([details.localPosition]);
+              }),
+              onPanUpdate: (details) => setState(() {
+                paths.last.add(details.localPosition);
+              }),
+              child: SizedBox(
+                child: CustomPaint(
+                  painter: Painter(paths),
+                  size: canvasSize,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        "人",
+                        style: GoogleFonts.kleeOne(
+                          fontSize: 80,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black26,
+                        ),
+                      ),
+                      Text(
+                        "人",
+                        style: GoogleFonts.kleeOne(
+                          fontSize: 80,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black26,
+                        ),
+                      ),
+                      Text(
+                        "人",
+                        style: GoogleFonts.kleeOne(
+                          fontSize: 80,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black26,
+                        ),
+                      ),
+                      Text(
+                        "人",
+                        style: GoogleFonts.kleeOne(
+                          fontSize: 80,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black26,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            SingleChildScrollView(
+              physics: AlwaysScrollableScrollPhysics(),
+              child: Column(
+                children: [
+                  defaultCard("Leitura", "Hito (ひと)", false),
+                  defaultCard("Vocabulario", "人間 (にんげん)", true),
+                  defaultCard("Vocabulario", "", true),
+                  defaultCard("Exemplo de uso", "", true),
+                ],
               ),
             ),
           ],
@@ -105,4 +185,46 @@ class _DrawingCanvasState extends State<DrawingCanvas> {
       ),
     );
   }
+}
+
+Widget defaultCard(String title, String description, bool hasImage) {
+  return SizedBox(
+    height: hasImage ? 170 : 100,
+    width: 340,
+    child: Card(
+      elevation: 0,
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            cardText(title, true),
+            cardText(description, false),
+            if (hasImage)
+              Padding(
+                padding: const EdgeInsets.only(top: 8),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [Icon(Icons.search, size: 62)],
+                ),
+              ),
+          ],
+        ),
+      ),
+    ),
+  );
+}
+
+Widget cardText(String text, bool isTitle) {
+  return Flexible(
+    child: Text(
+      text,
+      style: GoogleFonts.kleeOne(
+        fontSize: isTitle ? 18 : 24,
+        fontWeight: FontWeight.bold,
+        color: isTitle ? Colors.green.shade800 : Colors.black,
+      ),
+    ),
+  );
 }
